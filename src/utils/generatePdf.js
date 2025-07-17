77ce9c1e2cda676ef7ea572fa4abf7312de0a6c1
Roberto Berrio <robertocarlosberrio@gmail.com>
2025-07-17 13:11:32 -0500

    Configuration CORS

--- src/utils/generatePdf.js
+++ src/utils/generatePdf.js
@@ -57,7 +57,11 @@ export const generatePDF = async (data, templatePath) => {
     const context = { data };
     const finalHtml = template(context);
 
-    browser = await puppeteer.launch({ headless: true });
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    
     const page = await browser.newPage();
 
     await page.setContent(finalHtml, { waitUntil: "networkidle0" });
